5cbebdf87d627b1debd971e888146c710cbf87a6
Wassim <snaeler95@gmail.com>
2024-03-15 02:59:30 +0100

    Bonus done + json format for temperature request

--- locationEndPoint.py
+++ locationEndPoint.py
@@ -1,9 +1,18 @@
 from flask import Flask,request
 import requests
+import json
+from datetime import datetime
 import psycopg2 as pg2
+from pymeos import *
+from pymeos.db.psycopg2 import MobilityDB
 from psycopg2 import errors
-from psycopg2.errorcodes import UNIQUE_VIOLATION
+from psycopg2.errorcodes import UNIQUE_VIOLATION, INVALID_DATETIME_FORMAT
 from urllib.parse import quote, unquote
+
+app = Flask(__name__)
+
+#Initialize the library
+pymeos_initialize()
 
 conn = {
     'host': 'localhost',
@@ -20,17 +29,8 @@ try:
     # Create a cursor object to interact with the database
     cursor = connection.cursor()
 
-    # Now you can execute SQL queries using the cursor
-
-    # Example: Fetch and print the PostgreSQL version
-    cursor.execute("SELECT version();")
-    db_version = cursor.fetchone()
-    print("PostgreSQL version:", db_version)
-
 except (Exception, pg2.Error) as error:
     print("Error while connecting to PostgreSQL:", error)
-
-app = Flask(__name__)
 
 @app.route("/")
 def hello_world():
@@ -65,39 +65,61 @@ def hello():
         connection.rollback()
         return 'There is already an existing entry in the database with the current slug', 500
 
+def transform_forecasts(*forecast_tuples):
+    result = []
 
+    # Split the input data into min and max parts
+    min_forecasts = [item.split('@') for item in forecast_tuples[0].split(', ')]
+    max_forecasts = [item.split('@') for item in forecast_tuples[1].split(', ')]
+
+    # Iterate through min and max forecast data simultaneously
+    for min_data, max_data in zip(min_forecasts, max_forecasts):
+        date_min = datetime.strptime(min_data[1][:10], '%Y-%m-%d').date()
+
+        # Extract min and max forecast values
+        min_forecasted = float(min_data[0])
+        max_forecasted = float(max_data[0])
+
+        result.append({
+            "date": date_min.strftime('%Y-%m-%d'),
+            "min-forecasted": min_forecasted,
+            "max-forecasted": max_forecasted
+        })
+
+    return json.dumps(result, indent=2)
 
 
 @app.route("/cheminGet")
 def getTemp():
 
     slug = request.args.get('slug')
-    select_query = """
-                SELECT lon,lat 
-                FROM locations
-                WHERE slug = %s """
+    start_date = str(request.args.get('Start_date'))
+    end_date = str(request.args.get('End_date'))
+    select_query = f"""
+                SELECT attime(min_temperature,span('{start_date}'::timestamptz,'{end_date}'::timestamptz, true, true)),
+                    attime(max_temperature,span('{start_date}'::timestamptz,'{end_date}'::timestamptz, true, true)) 
+                FROM temp2mstore
+                WHERE slug = '{slug}'; """
 
-    cursor.execute(select_query, (slug,))
+    try :
+        cursor.execute(select_query)
 
-    result = cursor.fetchone()
-    if result:
-        api_url = f"https://www.7timer.info/bin/astro.php?lon={result[0]}&lat={result[1]}" \
-                  f"&ac=0&unit=metric&output=json&tzshift=0"
-        try:
-            # Make a GET request to the API endpoint
-            response = requests.get(api_url)
+        result = cursor.fetchone()
 
-            # Check if the request was successful (status code 200)
-            if response.status_code == 200:
-                # Parse and use the response data (in JSON format)
-                data = response.json()
-                print("API response:", data)
-                return data
-            else:
-                # Print an error message for unsuccessful requests
-                print(f"Error: {response.status_code} - {response.text}")
+        print(result)
+        if result != (None,None):
+            result = (result[0][1:-1],result[1][1:-1])
+            print(result)
+            result = transform_forecasts(*result)
+            return result, 200
+        else :
+            return "There is no data available for time range requested", 500
+    except errors.lookup(UNIQUE_VIOLATION) as e:
+        # Handle the unique key constraint violation error here
+        # You can print an error message, log the error, or take other appropriate actions
+        print(f"Error: {e}")
+        return 'The datatime format is not correct it should be as follow YYYY-MM-DD', 500
 
-        except requests.RequestException as e:
-            print("Error during API request:", e)
-    else:
-        return 'There is no location stored in the databse for the given slug', 500
+
+if __name__ == '__main__':
+    app.run()
